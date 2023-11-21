ba5cc2a28f5e4da89d2a89b07afb7766df803355
harshitha-36 <128170209+harshitha-36@users.noreply.github.com>
2023-11-21 23:27:00 +0530

    Update Welcome.js

--- src/component/Welcome.js
+++ src/component/Welcome.js
@@ -1,22 +1,13 @@
 import { Grid, Typography } from "@material-ui/core";
+import videobg from '../play/video2.mp4'
 
-const Welcome = (props) => {
+const Welcome =() => {
   return (
-    <Grid
-      container
-      item
-      direction="column"
-      alignItems="center"
-      justify="center"
-      style={{ padding: "30px", minHeight: "93vh" }}
-    >
-      <Grid item>
-        <Typography variant="h2">Welcome to Job Portal</Typography>
-      </Grid>
-    </Grid>
-  );
-};
-
+    <div className='welcome'>
+      <video src ={videobg} autoPlay loop muted type="video/mp4" />
+    </div>
+  )
+}
 export const ErrorPage = (props) => {
   return (
     <Grid
